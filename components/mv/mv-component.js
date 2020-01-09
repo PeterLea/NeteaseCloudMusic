0dfa18e385a21d04acb81fddc46eedabacbcad05
coldplayzy <1733139936@qq.com>
2020-01-09 14:04:11 +0800

    第二次提交

---
+++ components/mv/mv-component.js
@@ -0,0 +1,28 @@
+// components/mv/mv-component.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+  list:Array
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    lt:'<<',
+    gt:'>>'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    bindscrolltolower(){
+      console.log(0);
+      
+    }
+  
+  }
+})
